af7798a12f7503257b1688356b9dc79a5fcc1c00
JonathAM20 <jonathan_msi@live.com>
2021-08-23 09:14:31 -0400

    Implementation of data binding

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -1,10 +1,13 @@
 package com.example.controller;
 
+import com.example.model.SignupForm;
 import com.example.service.UserApplicationService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,20 +16,23 @@ import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
+@Slf4j
 public class SignupController {
 
     @Autowired
     private UserApplicationService userApplicationService;
 
     @GetMapping("signup")
-    public String getSignup(Model model, Locale locale){
+    public String getSignup(Model model, Locale locale, @ModelAttribute SignupForm form){
         Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
         model.addAttribute("genderMap", genderMap);
         return "user/signup";
     }
 
     @PostMapping("signup")
-    public String postSignup(){
+    public String postSignup(@ModelAttribute SignupForm form){
+        log.info(form.toString());
+        System.out.println(form.toString());
         return "redirect:/login";
     }
 }
